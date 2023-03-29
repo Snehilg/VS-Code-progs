ce7467eea8bbb4abb0858687a591929f0b4a719a
Snehil <snehilpg87@gmail.com>
2023-03-29 14:34:28 +0530

    final merge sort code

--- DSA/sorting/MergeSort.java
+++ DSA/sorting/MergeSort.java
@@ -12,7 +12,7 @@ class MergeSort{
             arr[i]=Integer.parseInt(s[i]);
 
         merSort(arr,0,n-1);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("ans->"+Arrays.toString(arr));
     }
 
     static void merSort(int arr[],int l,int r){
@@ -31,23 +31,31 @@ class MergeSort{
         int n1=m-l+1; int n2=r-m;
         int left[]=new int[n1];
         int right[]=new int[n2];
-
+        
+        System.out.println("=====================");
+        System.out.println("arr before merge"+Arrays.toString(arr));
+        
         for(int i=0;i<n1;i++)         //copying arr from l->m
             left[i]=arr[l+i];
         
-        for(int i=0;i<n2;i++)         //copying arr from m+1->r
-            right[i]=arr[n1+i];       //using n1
+        System.out.println("left->"+Arrays.toString(left));
 
-        int i=0,j=0,k=0;
+        for(int i=0;i<n2;i++)         //copying arr from m+1->r
+            right[i]=arr[m+1+i];       //using n1
+
+        System.out.println("right->"+Arrays.toString(right));
+
+        int i=0,j=0,k=l;
 
         while(i<n1 && j<n2){        //merging
             if(left[i]<=right[j]){
                 arr[k]=left[i];
-                i++; k++;
+                i++; 
             }else{
                 arr[k]=right[j];
-                j++; k++;
+                j++;
             }
+            k++;
         }//while
 
         while(i<n1){
@@ -59,6 +67,9 @@ class MergeSort{
             arr[k]=right[j];
             j++; k++;
         }
+
+        System.out.println("arr after merge"+Arrays.toString(arr));
+        System.out.println("=====================");
 
     }//merge function
 
